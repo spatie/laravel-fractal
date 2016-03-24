73f09b51a7cfeef0bb759d8bb82c19c6280a4a9a
angel@europasat.com <angel@europasat.com>
2016-03-24 15:57:45 +0000

    Added tests for Cursor

--- tests/Unit/FractalTest.php
+++ tests/Unit/FractalTest.php
@@ -5,6 +5,7 @@ namespace Spatie\Fractal\Test;
 use League\Fractal\Manager;
 use Spatie\Fractal\ArraySerializer;
 use Spatie\Fractal\Fractal;
+use \League\Fractal\Pagination\Cursor;
 
 class FractalTest extends \PHPUnit_Framework_TestCase
 {
@@ -27,6 +28,9 @@ class FractalTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf(get_class($this->fractal), $this->fractal->collection([]));
         $this->assertInstanceOf(get_class($this->fractal), $this->fractal->transformWith(function () {}));
         $this->assertInstanceOf(get_class($this->fractal), $this->fractal->serializeWith(new ArraySerializer()));
+        $this->assertInstanceOf(get_class($this->fractal), $this->fractal->withCursor(
+            new Cursor(0, null, 10, 10)
+        ));
         $this->assertInstanceOf(get_class($this->fractal), $this->fractal->addMeta([]));
         $this->assertInstanceOf(get_class($this->fractal), $this->fractal->paginateWith(
             $this->getMock('League\Fractal\Pagination\PaginatorInterface')
