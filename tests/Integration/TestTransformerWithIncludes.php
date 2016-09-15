01cf66f5e70f4b97f6b4ae3e32e01d31dbd80d14
morloderex <michael.lundboel@gmail.com>
2016-09-15 20:14:57 +0000

    Applied fixes from StyleCI

--- tests/Integration/TestTransformerWithIncludes.php
+++ tests/Integration/TestTransformerWithIncludes.php
@@ -14,7 +14,7 @@ class TestTransformerWithIncludes extends TransformerAbstract
     protected $defaultIncludes = [
         'characters',
         'publisher',
-        'title'
+        'title',
     ];
 
     /**
