b05fb489c362c3f0df3238c6245d2b2499c12dc3
Freek Van der Herten <freek@spatie.be>
2016-09-23 21:53:52 +0000

    Applied fixes from StyleCI

--- src/Exceptions/InvalidUseOfFractalHelper.php
+++ src/Exceptions/InvalidUseOfFractalHelper.php
@@ -13,7 +13,7 @@ class InvalidUseOfFractalHelper extends Exception
 
     public static function tooManyArguments(array $arguments)
     {
-        $argumentCount  = count($arguments);
+        $argumentCount = count($arguments);
 
         return new static("You passed {$argumentCount} to `fractal()`. The maximum amount of arguments is 3.");
     }
