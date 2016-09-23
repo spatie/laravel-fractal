2aae2fcd69577a51bad1b6922024d74984135344
Freek Van der Herten <freek@spatie.be>
2016-09-24 00:11:52 +0200

    Applied fixes from StyleCI (#43)

--- src/helpers.php
+++ src/helpers.php
@@ -3,7 +3,6 @@
 use Spatie\Fractal\FractalFunctionHelper;
 
 if (! function_exists('fractal')) {
-    
     function fractal()
     {
         $fractalFunctionHelper = new FractalFunctionHelper(func_get_args());
