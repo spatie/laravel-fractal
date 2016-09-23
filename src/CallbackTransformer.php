265544563d51697833fdcc58ff11aec30277d5b7
freek <freek@spatie.be>
2016-09-24 00:24:05 +0200

    fix PHP 5.6 compat

--- src/CallbackTransformer.php
+++ src/CallbackTransformer.php
@@ -21,6 +21,8 @@ class CallbackTransformer extends TransformerAbstract
      */
     public function transform($item)
     {
-        return ($this->callable)($item);
+        $callable = $this->callable;
+
+        return $callable($item);
     }
 }
