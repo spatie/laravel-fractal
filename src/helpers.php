54ac48fdda6e2fde8006791c113a20db98a65869
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-09-27 10:49:09 +0200

    Phpdoc on the helper function

--- src/helpers.php
+++ src/helpers.php
@@ -3,6 +3,13 @@
 use Spatie\Fractal\FractalFunctionHelper;
 
 if (! function_exists('fractal')) {
+    /**
+     * @param mixed $data
+     * @param callable|\League\Fractal\TransformerAbstract $transformer
+     * @param \League\Fractal\Serializer\SerializerAbstract $serializer
+     *
+     * @return \Spatie\Fractal\Fractal
+     */
     function fractal()
     {
         $fractalFunctionHelper = new FractalFunctionHelper(func_get_args());
