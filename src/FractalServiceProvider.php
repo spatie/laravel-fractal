cb665dcad43f990d98840c81c228166a5052563d
Robin van der Vleuten <robin@webstronauts.co>
2017-02-22 00:15:06 +0100

    Made service provider compatible with Lumen (#103)
    * Made service provider compatible with Lumen
    
    * Fixed StyleCI comments

--- src/FractalServiceProvider.php
+++ src/FractalServiceProvider.php
@@ -3,7 +3,9 @@
 namespace Spatie\Fractal;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 use Spatie\Fractal\Console\Commands\TransformerMakeCommand;
+use Illuminate\Foundation\Application as LaravelApplication;
 
 class FractalServiceProvider extends ServiceProvider
 {
@@ -12,9 +14,7 @@ class FractalServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([
-            __DIR__.'/../resources/config/laravel-fractal.php' => config_path('laravel-fractal.php'),
-        ], 'config');
+        $this->setupConfig();
 
         if ($this->app->runningInConsole()) {
             $this->commands([
@@ -28,12 +28,24 @@ class FractalServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../resources/config/laravel-fractal.php', 'laravel-fractal');
-
         $this->app->singleton('laravel-fractal', function (...$arguments) {
             return fractal(...$arguments);
         });
 
         $this->app->alias('laravel-fractal', Fractal::class);
+    }
+
+    protected function setupConfig()
+    {
+        $source = realpath(__DIR__.'/../resources/config/laravel-fractal.php');
+
+        if ($this->app instanceof LaravelApplication) {
+            $this->publishes([$source => config_path('laravel-fractal.php')]);
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('laravel-fractal');
+
+        }
+
+        $this->mergeConfigFrom($source, 'laravel-fractal');
     }
 }
