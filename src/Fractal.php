93b142145590013bbba2af6adeef054279650454
Chris Stretton <cstretton@gmail.com>
2017-09-07 16:07:22 +0100

    Exposed the underlying JsonResponse object's encoding options

--- src/Fractal.php
+++ src/Fractal.php
@@ -79,10 +79,11 @@ class Fractal extends Fractalistic
      *
      * @param  callable|int $statusCode
      * @param  callable|array $headers
+     * @param  callable|int $options
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function respond($statusCode = 200, $headers = [])
+    public function respond($statusCode = 200, $headers = [], $options = 0)
     {
         $response = new JsonResponse();
 
@@ -98,6 +99,17 @@ class Fractal extends Fractalistic
             $headers = function (JsonResponse $response) use ($headers) {
                 return $response->withHeaders($headers);
             };
+        }
+
+        if (is_int($options)) {
+            $options = function (JsonResponse $response) use ($options) {
+                $response->setEncodingOptions($options);
+                return $response;
+            };
+        }
+
+        if (is_callable($options)) {
+            $options($response);
         }
 
         if (is_callable($statusCode)) {
