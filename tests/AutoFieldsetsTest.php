3134ca2ef69d85727777403dcc6030446637e14d
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-12-09 15:56:16 +0330

    Allow auto parsing the `fieldsets` (#261)
    * fix: wrong `return` annotation
    
    * fix: wrong return type annotation
    
    * feat: auto-parse fieldsets
    
    * Fix styling
    
    ---------
    
    Co-authored-by: Mohammad-Alavi <Mohammad-Alavi@users.noreply.github.com>

---
+++ tests/AutoFieldsetsTest.php
@@ -0,0 +1,108 @@
+<?php
+
+use Illuminate\Testing\Fluent\AssertableJson;
+
+it('returns only requested fields', function ($fields, $expectedMissing) {
+    $response = $this->call('GET', '/auto-fieldsets-with-resource-name', [
+        'fields' => [
+            'books' => $fields,
+        ],
+        'include' => 'characters,publisher',
+    ]);
+
+    $response->assertOk();
+    $response->assertJson(
+        fn (AssertableJson $json) => $json->has(
+            'data',
+            2,
+            fn (AssertableJson $json) => $json->hasAll($fields)
+            ->missing($expectedMissing),
+        ),
+    );
+})->with([
+    ['author', 'id'],
+    ['id', 'author'],
+    [['author', 'id'], 'publisher'],
+    [['id', 'author', 'publisher'], 'characters'],
+]);
+
+it('doesnt work if "resource name" is not set', function ($fields) {
+    $response = $this->call('GET', '/auto-fieldsets-without-resource-name', [
+        'fields' => [
+            'books' => $fields,
+        ],
+        'include' => 'characters,publisher',
+    ]);
+
+    $response->assertOk();
+    $response->assertJson(
+        fn (AssertableJson $json) => $json->has(
+            'data',
+            2,
+            fn (AssertableJson $json) => $json->hasAll(['id', 'author', 'characters', 'publisher'])
+        ),
+    );
+})->with([
+    ['author', 'id'],
+    ['id', 'author'],
+    [['author', 'id']],
+    [['id', 'author', 'publisher']],
+]);
+
+it('all fields are present when parameter is not passed', function () {
+    $response = $this->call('GET', '/auto-fieldsets-with-resource-name', [
+        'include' => 'characters',
+    ]);
+
+    $response->assertOk();
+    $response->assertJson(
+        fn (AssertableJson $json) => $json->has(
+            'data',
+            2,
+            fn (AssertableJson $json) => $json->hasAll(['id', 'author', 'characters'])
+            ->missing('publisher'),
+        ),
+    );
+});
+
+it('can be disabled via config', function () {
+    config()->set('fractal.auto_fieldsets.enabled', false);
+
+    $response = $this->call('GET', '/auto-fieldsets-with-resource-name', [
+        'fields' => [
+            'books' => ['author', 'id'],
+        ],
+        'include' => 'characters,publisher',
+    ]);
+
+    $response->assertOk();
+    $response->assertJson(
+        fn (AssertableJson $json) => $json->has(
+            'data',
+            2,
+            fn (AssertableJson $json) => $json->hasAll(['id', 'author', 'characters', 'publisher'])
+        ),
+    );
+});
+
+it('uses the configured request key', function () {
+    config()->set('fractal.auto_fieldsets.request_key', 'other_fields');
+    $response = $this->call('GET', '/auto-fieldsets-with-resource-name', [
+        'fields' => [
+            'books' => 'author',
+        ],
+        'other_fields' => [
+            'books' => 'id',
+        ],
+    ]);
+
+    $response->assertOk();
+    $response->assertJson(
+        fn (AssertableJson $json) => $json->has(
+            'data',
+            2,
+            fn (AssertableJson $json) => $json->has('id')
+            ->missing('author')
+        ),
+    );
+});
